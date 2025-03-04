766ae68d3d5a5fc1f72b4c188243693be710609b
Patrick Meaney <patrick.wm.meaney@gmail.com>
2025-03-04 12:43:48 -0800

    trying different prod vars

--- wagtail_cms_portfolio2025/wagtail_cms_portfolio2025/settings/production.py
+++ wagtail_cms_portfolio2025/wagtail_cms_portfolio2025/settings/production.py
@@ -2,6 +2,19 @@ from .base import *
 
 DEBUG = False
 
+SECRET_KEY = "django-insecure-h3+7&(bxg0y0%-61gre3wb$khtusvqp*bb1w74r0cf1coa=mb6"
+
+ALLOWED_HOSTS = [
+    'cms.livestauction.com', 
+    '127.0.0.1', 
+    'localhost'
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://cms.livestauction.com',
+    'http://cms.livestauction.com'
+]
+
 try:
     from .local import *
 except ImportError:
